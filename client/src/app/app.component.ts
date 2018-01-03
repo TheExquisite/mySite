0c68fe76d14d292e67b93acfbf2c3b98c744902e
Exquisite <jcjamescclare@gmail.com>
2018-01-03 15:27:31 +1000

    animation framework set up

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { OnInit, AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit, AfterViewInit{
+  private canvas: HTMLCanvasElement;
+  frameRate = 1000/60;
+  t: number;
+
+  constructor(){}
+  ngOnInit(){}
+
+  ngAfterViewInit(){
+    let canvas = <HTMLCanvasElement> document.getElementById("backgroundCanvas");
+    if(canvas != null){
+      this.t = 0;
+      let animationInterval = setInterval(() => this.animate(canvas), this.frameRate);
+      console.log(":) canvas is not null");
+    } else {
+      console.log(":( canvas is null");
+    }
+  }
+
+  animate(c:HTMLCanvasElement){
+    this.t++;
+    console.log(this.t);
+    let x = c.getContext('2d');
+    c.width = c.clientWidth;
+    c.height = c.clientHeight;
+    x.fillStyle = "#FF0000";
+    let q=10000;
+    for(let i=1;i<q;i++){
+      let r=10+i*0.01;
+      x.fillRect(960+i*Math.cos(i),540+i*0.5+Math.cos(Math.cos(this.t*0.006)*i*0.015)*80,r,r);
+
+    }
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,3 +18,11 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+    // let q=10000;
+    // for(let i=1;i<q;i++){
+    //   let r=10+i*0.01;
+    //   x.fillRect(960+i*Math.cos(i),540+i*0.5+Math.cos(Math.cos(this.t*0.6)*i*0.015)*80,r,r);
+
+    // }
